98235443d6eb479755cb9581989ff1edb125be6a
A-Talha <96351797+A-Talha@users.noreply.github.com>
2022-12-28 22:03:43 +0200

    login and register

--- front-end/src/app/nav-bar/nav-bar.component.ts
+++ front-end/src/app/nav-bar/nav-bar.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
-
+export class NavBarComponent implements OnInit{
+  empage = false;
+  constructor(
+      private rouv :ActivatedRoute
+  ) {
+  }
+  ngOnInit() {
+    this.rouv.queryParams.subscribe((params:any)=>{
+      //console.log(params.data1);
+      if (params.data1==="true"||params.data2==="true"){
+        // @ts-ignore
+        document.getElementById("nav").style.display = "block";
+      }
+      else {
+        // @ts-ignore
+        document.getElementById("nav").style.display = "none";
+      }
+    });
+  }
 }
