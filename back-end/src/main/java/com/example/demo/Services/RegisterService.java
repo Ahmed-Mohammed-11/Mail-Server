0874c52c29535476baa0e9d098cdc089d56bbc2d
A-Talha <96351797+A-Talha@users.noreply.github.com>
2022-12-28 22:33:07 +0200

    Revert "Merge branch 'main' of https://github.com/Ahmed-Mohammed-11/Mail-Server"
    This reverts commit 35eedc25a8eb5c6f422877dff0d7b34506c6c566, reversing
    changes made to 98235443d6eb479755cb9581989ff1edb125be6a.

---
+++ back-end/src/main/java/com/example/demo/Services/RegisterService.java
@@ -0,0 +1,71 @@
+package com.example.demo.Services;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.stereotype.Service;
+import java.io.*;
+import java.util.UUID;
+import javax.servlet.http.HttpServletResponse;
+
+@Service
+public class RegisterService {
+    //try to change to relative path instead of the relative path
+    String filePath = System.getProperty("user.dir") + "\\src\\main\\java\\com\\example\\demo\\Database\\";
+//    private String filePath = "D:\\Web\\Mail-Server\\back-end\\src\\main\\java\\com\\example\\demo\\DataBase\\";
+    // if the relative path didn't work uncomment the absolute path
+
+    public String createUser(String user, HttpServletResponse response) throws IOException, ParseException {
+
+        //convert the user from string to json object
+        JSONObject userJson = new JSONObject(user);
+        JSONParser parser  = new JSONParser();
+        //try reading the users.json file
+        FileReader file = new FileReader(filePath + "users.json");
+        //parse the previous content of the users.json file and store it in object loaded
+        Object loaded =  parser.parse(file);
+        //convert the object to json array to be able to traverse and add the new object to it
+        JSONArray a = new JSONArray(loaded.toString());
+        //loop through current users to check if the username was already taken
+        for(int i = 0 ; i < a.length() ; i ++){
+            JSONObject elementInArray = a.getJSONObject(i);
+            if (elementInArray.get("email").equals(userJson.get("email"))){
+                //return the error if it's already taken
+                return "this username is already taken";
+            }
+        }
+        //generate random user uuid
+        UUID uuid = UUID.randomUUID();
+        //if you managed to pass the loop put the generated uuid as a new entry in the user object
+        userJson.put("uuid", uuid);
+        //add the object in the array
+        a.put(userJson);
+        //try to write your changes to users.json file
+        try {
+            FileWriter users = new FileWriter(filePath + "users.json");         // writing back to the file
+            users.write(a.toString());
+            users.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        //create some directories for that user
+        File newUserFolder = new File(filePath + uuid);
+        File inbox = new File(filePath + uuid + "\\inbox");
+        File sent = new File(filePath + uuid + "\\sent");
+        File drafts = new File(filePath + uuid + "\\drafts");
+        File trash = new File(filePath + uuid + "\\trash");
+        newUserFolder.mkdir();
+        inbox.mkdir();
+        sent.mkdir();
+        drafts.mkdir();
+        trash.mkdir();
+
+
+
+        //and as an indicator return the created session id for him
+        response.setHeader("Set-Cookie", "user_id=" + uuid.toString() + "; Path=/");
+        return  uuid.toString();
+    }
+}
