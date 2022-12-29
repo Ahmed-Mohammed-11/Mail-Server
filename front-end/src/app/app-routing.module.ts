ae987ef55e144c70b6ae699280b27ef5d9841146
A-Talha <96351797+A-Talha@users.noreply.github.com>
2022-12-29 21:26:35 +0200

    Revert "talha login"
    This reverts commit d405118dea96311a053c49719ebbc6f8b593e399.

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -10,7 +10,6 @@ const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: '', component: NavBarComponent},
-  {path: 'nav-bar', component: NavBarComponent},
 ];
 
 @NgModule({
