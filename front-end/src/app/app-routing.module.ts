4d3ed0b291612a4a918e5021f3a100fc9541f18d
A-Talha <96351797+A-Talha@users.noreply.github.com>
2022-12-29 21:37:22 +0200

    RE

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: '', component: NavBarComponent},
+  {path: 'nav-bar', component: NavBarComponent},
 ];
 
 @NgModule({
